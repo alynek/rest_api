8c3d189cf7875380e4eab8a44f6dfab3c8bc467a
alynek <alyne0x67@gmail.com>
2022-02-21 23:53:19 -0300

    Create automapper configuration file and dependency injection configuration file

---
+++ src/Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using DevIO.Api.Dtos;
+using DevIO.Business.Models;
+
+namespace Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorDto>().ReverseMap();
+            CreateMap<Endereco, EnderecoDto>().ReverseMap();
+            CreateMap<Produto, ProdutoDto>().ReverseMap();
+        }
+    }
+}
